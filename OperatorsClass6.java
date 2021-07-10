bfb2eab23c3fbc60cb96cea3fe6488cc7aca6ab6
CodeWithKartikk <84595018+CodeWithKartikk@users.noreply.github.com>
2021-07-11 01:48:28 +0530

    Operators Intro in Java

---
+++ OperatorsClass6.java
@@ -0,0 +1,26 @@
+public class OperatorsClass6 {
+    // Java Operators
+    // Operators : Operators are used to perform operations on variables and values
+    // a = 5, b = 7, c = a + b => c = 5 + 7 (performing operations on variables and
+    // values)
+    public static void main(String[] args) {
+        int x = 100 + 50;
+        System.out.println(x);   
+
+        int sum1 = 100 + 50;
+        int sum2 = sum1 + 250;
+        int sum3 = sum2 + sum2;
+        System.out.println(sum1);
+        System.out.println(sum2);
+        System.out.println(sum3);
+
+        /*
+        Java divides the operators into the following groups:
+           > Arithmetic Operators
+           > Assignment Operators
+           > Comparison Operators
+           > Logical Operators
+           > Bitwise Operators
+        */
+    }
+}
