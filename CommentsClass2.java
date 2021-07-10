480bd46e410ef7e0f0883b24348584cfbce45b4d
CodeWithKartikk <84595018+CodeWithKartikk@users.noreply.github.com>
2021-07-11 01:42:11 +0530

    Comments

---
+++ CommentsClass2.java
@@ -0,0 +1,27 @@
+//  Java Syntax
+// Definition Class - if we don't have a main method then its  a definition class
+// Execution Class - If we have a main method present over there then its execution class.
+
+public class CommentsClass2 // Class
+{
+    public static void main(String[] args) // Main Method
+    {  
+        // Print statement in Java - System.out.println();
+        // ; --> Line terminator
+        System.out.println("Hello Java"); 
+
+        // Java Comments - Two Types of comments in Java
+           // 1. Single Line Comment 
+           // 2. Multi Line Comment
+
+           // Single Line Comment
+           // Ishaan wants to be a Java Programmer and he wants to make mobile app to learn Java for other students
+        
+           // Multi Line Comment
+           /* Ishaan wants to be a Java Programmer 
+           and he wants to make 
+           mobile app to learn 
+           Java for other students
+           */
+    }
+}
