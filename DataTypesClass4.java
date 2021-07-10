a170bcc2d52ea5b78be118c13cda8db3b836a3c9
CodeWithKartikk <84595018+CodeWithKartikk@users.noreply.github.com>
2021-07-11 01:45:39 +0530

    DataTypes in Java

---
+++ DataTypesClass4.java
@@ -0,0 +1,45 @@
+public class DataTypesClass4 {
+    // You have to create 10 variales by using all the datatypes
+    public static void main(String[] args) {
+        int intVar = 5;
+        int intVar1 = 596;
+        float floatVar = 5963.44f;
+        float floatVar1 = 995.0954f;
+        String stVariable = "Kartik";
+        String stVariable1 = "Ishaan";
+        boolean boolVariable = true;
+        boolean boolVariable1 = false;
+        char charVariable = 'A';
+        char charVariable1 = 'H';
+        System.out.println(intVar);
+        System.out.println(intVar1);
+        System.out.println(floatVar);
+        System.out.println(floatVar1);
+        System.out.println(stVariable);
+        System.out.println(stVariable1);
+        System.out.println(boolVariable);
+        System.out.println(boolVariable1);
+        System.out.println(charVariable);
+        System.out.println(charVariable1);
+
+        // Java Data Types
+        // int, float, String, char, boolean
+
+        // DataTyes are divided into two groups
+        // 1. Primitive DataTypes
+        // > DataTypes      Size(in bytes)
+        // > byte        -       1             
+        // > short       -       2            
+        // > int         -       4
+        // > long        -       8
+        // > float       -       4
+        // > double      -       8
+        // > boolean     -       1(bit)  
+        // > char        -       2
+
+        // 2. Non Primitive DataTypes and Reference DataTypes
+        // > String   (String = "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
+        // > Arrays   (int[] array = {1,2,3,4,5,6,7,8,9,10-------n})
+        // > Classes   
+    }
+}
