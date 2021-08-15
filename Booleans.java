15074a6393f983d98adbc4f158458ba06edfd055
CodeWithKartikk <84595018+CodeWithKartikk@users.noreply.github.com>
2021-08-15 13:50:40 +0530

    Booleans in Java

---
+++ Booleans.java
@@ -0,0 +1,22 @@
+public class JavaBooleans {
+    public static void main(String[] args) {
+        // Yes/ No, True/False, On/Off
+
+        boolean isJavaFun = true;
+        System.out.println(isJavaFun);
+
+        boolean isAIFun = true;
+        System.out.println(isAIFun);
+
+        int x = 10;
+        int y = 5;
+        System.out.println(x > y);
+
+        System.out.println(10 > 24);
+
+        int z = 10;
+        System.out.println(z == 10);
+
+        System.out.println(10 == 15);
+    }
+}
