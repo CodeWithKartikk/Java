385d0e5bad36fe369eb63ea10f6b1bf03a7bbc09
CodeWithKartikk <84595018+CodeWithKartikk@users.noreply.github.com>
2021-08-15 13:50:05 +0530

    Java Math

---
+++ Math.java
@@ -0,0 +1,18 @@
+public class JavaMath {
+    public static void main(String[] args) {
+        // Math.max(a,b)
+        System.out.println(Math.max(5, 10));
+
+        // Math.min(a,b)
+        System.out.println(Math.min(5, 10));
+
+        // Math.sqrt(x)
+        System.out.println(Math.sqrt(64));
+
+        // Math.abs(x)
+        System.out.println(Math.abs(-7.85));
+
+        // Random Numbers
+        System.out.println(Math.random());
+    }
+}
